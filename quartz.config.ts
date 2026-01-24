699c95f9a8aeef9ce505dc7ec982cc587e670602
liuwei <pseudoway@163.com>
2026-01-24 13:53:57 +0800

    修改配置

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "zh-CN",
+    baseUrl: "hushway.github.io/blog/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
